4b416966611453c98466d62d9508178e630c0d40
Serban Iordache <iordache@hotmail.com>
2016-06-29 14:43:29 +0200

    bump to version 1.0.10-SNAPSHOT

--- travis-build.sh
+++ travis-build.sh
@@ -2,6 +2,11 @@
 set -ev
 ./gradlew build asciidoc
 if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
-  ./gradlew publishGhPages --rerun-tasks
-  ./gradlew publishGhPages --rerun-tasks -PversionedGhPage=true
+  git show-ref --verify --quiet refs/heads/gh-pages
+  if [ $? -ne 0 ]
+  then
+    ./gradlew publishGhPages --rerun-tasks -PghPageType=init
+  fi
+  ./gradlew publishGhPages --rerun-tasks -PghPageType=latest
+  ./gradlew publishGhPages --rerun-tasks -PghPageType=version
 fi
