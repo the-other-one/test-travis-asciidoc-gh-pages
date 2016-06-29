24e7d213d15a6a4de883a92c389537f3169e5c55
Serban Iordache <iordache@hotmail.com>
2016-06-29 12:27:38 +0200

    force executing the publishGhPages tasks

--- travis-build.sh
+++ travis-build.sh
@@ -2,6 +2,6 @@
 set -ev
 ./gradlew build asciidoc
 if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
-  ./gradlew publishGhPages
-  ./gradlew publishGhPages -PversionedGhPage=true
+  ./gradlew publishGhPages --rerun-tasks
+  ./gradlew publishGhPages --rerun-tasks -PversionedGhPage=true
 fi
