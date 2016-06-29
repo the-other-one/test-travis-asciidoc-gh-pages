212d98a0834fad4a053122b3170cb75d5c6ac535
Serban Iordache <iordache@hotmail.com>
2016-06-29 08:53:01 +0200

    fix travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 ./gradlew build asciidoc
-if [ ("${TRAVIS_PULL_REQUEST}" = "false") -a ("${TRAVIS_BRANCH}" = "master") ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
   ./gradlew publishGhPages
   ./gradlew publishGhPages -PversionedGhPage=true
 fi
