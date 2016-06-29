aeae679574351a754b46d6680cff2ed31096f990
Serban Iordache <iordache@hotmail.com>
2016-06-29 08:45:20 +0200

    call publishGhPages twice

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ev
 ./gradlew build asciidoc
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ ("${TRAVIS_PULL_REQUEST}" = "false") -a ("${TRAVIS_BRANCH}" = "master") ]; then
   ./gradlew publishGhPages
+  ./gradlew publishGhPages -PversionedGhPage=true
 fi
