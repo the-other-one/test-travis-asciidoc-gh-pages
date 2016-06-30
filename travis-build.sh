7b98a365884a4560946e3bf5327af8e58b182063
Serban Iordache <iordache@hotmail.com>
2016-06-30 11:08:10 +0200

    add debug messages

--- travis-build.sh
+++ travis-build.sh
@@ -5,8 +5,14 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
   git show-ref --verify --quiet refs/heads/gh-pages
   if [ $? -ne 0 ]
   then
+    echo publishGhPages init started
     ./gradlew publishGhPages --rerun-tasks -PghPageType=init
+    echo publishGhPages init finished
   fi
+  echo publishGhPages latest started
   ./gradlew publishGhPages --rerun-tasks -PghPageType=latest
+  echo publishGhPages latest finished
+  echo publishGhPages version started
   ./gradlew publishGhPages --rerun-tasks -PghPageType=version
+  echo publishGhPages version finished
 fi
