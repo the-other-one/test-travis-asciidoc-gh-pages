796afd746e6ce18402cf48a0a52f2b56b81b2b25
Serban Iordache <iordache@hotmail.com>
2016-06-30 14:40:42 +0200

    release 1.0.13

--- travis-build.sh
+++ travis-build.sh
@@ -3,23 +3,12 @@ set -ev
 ./gradlew build asciidoc
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
-  echo executing git ls-remote
-  git ls-remote
-  echo .
-
-  echo executing git ls-remote origin
-  git ls-remote origin
-  echo .
-
   echo executing git ls-remote origin gh-pages
   git ls-remote origin gh-pages
   echo .
 
-  echo executing set exists
-
-  set exists = `git ls-remote origin gh-pages`
-  echo exists: "$exists"
-  if [ "$exists" == "" ]; then
+  echo executing if
+  if [ -n `git ls-remote origin gh-pages` ]; then
     echo Branch gh-pages does not exists.
     ./gradlew publishGhPages --rerun-tasks -PghPageType=init
   fi
