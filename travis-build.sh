b765ab13262f87fda3a09585e55259d8f73209da
Serban Iordache <iordache@hotmail.com>
2016-06-30 13:33:03 +0200

    print debug messages

--- travis-build.sh
+++ travis-build.sh
@@ -5,6 +5,7 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
   set exists = `git show-ref refs/heads/gh-pages`
   if [ -n $exists ]; then
+    echo Branch gh-pages does not exists.
     ./gradlew publishGhPages --rerun-tasks -PghPageType=init
   fi
   ./gradlew publishGhPages --rerun-tasks -PghPageType=latest
