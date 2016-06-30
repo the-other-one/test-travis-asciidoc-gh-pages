cee1fd6a24924a912b71b0760aa4643b85102804
Serban Iordache <iordache@hotmail.com>
2016-06-30 14:56:33 +0200

    bump version to 1.0.14-SNAPSHOT

--- travis-build.sh
+++ travis-build.sh
@@ -3,12 +3,11 @@ set -ev
 ./gradlew build asciidoc
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
-  echo executing git ls-remote origin gh-pages
-  git ls-remote origin gh-pages
+  echo executing git ls-remote origin gh-pages: "`git ls-remote origin gh-pages`"
   echo .
 
   echo executing if
-  if [[ -n `git ls-remote origin gh-pages` ]]; then
+  if [ "`git ls-remote origin gh-pages`" == "" ]]; then
     echo Branch gh-pages does not exists.
     ./gradlew publishGhPages --rerun-tasks -PghPageType=init
   fi
