b9afa9d1fcc5ad7af0202cab04424770a0e3a9c8
Serban Iordache <iordache@hotmail.com>
2016-06-30 12:58:22 +0200

    try to fix the build

--- travis-build.sh
+++ travis-build.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 set -ev
-echo asciidoc started
 ./gradlew build asciidoc
-echo asciidoc finished
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
-  echo git show-ref started
   set exists = `git show-ref refs/heads/gh-pages`
-  echo exists: @$exists@
   if [ -n $exists ]; then
-    echo gh-pages branch does not exist
     ./gradlew publishGhPages --rerun-tasks -PghPageType=init
-    echo publishGhPages init finished
-  else
-    echo gh-pages branch already exists
   fi
-  echo publishGhPages latest started
   ./gradlew publishGhPages --rerun-tasks -PghPageType=latest
-  echo publishGhPages latest finished
-  echo publishGhPages version started
   ./gradlew publishGhPages --rerun-tasks -PghPageType=version
-  echo publishGhPages version finished
 fi
