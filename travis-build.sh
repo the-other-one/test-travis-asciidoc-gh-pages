0935a16b916b5b3d577646d59cf13d43553b8bcd
Serban Iordache <iordache@hotmail.com>
2016-09-20 16:01:07 +0200

    logging

--- travis-build.sh
+++ travis-build.sh
@@ -4,8 +4,18 @@ set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
   if [ "`git ls-remote origin gh-pages`" == "" ]; then
+    echo '****************************************************'
+    echo '***** Executing publishGhPages with PghPageType=init'
+    echo '****************************************************'
     ./gradlew --no-daemon publishGhPages --rerun-tasks --info --stacktrace -PghPageType=init
   fi
+  echo '******************************************************'
+  echo '***** Executing publishGhPages with PghPageType=latest'
+  echo '******************************************************'
   ./gradlew --no-daemon publishGhPages --rerun-tasks --info --stacktrace -PghPageType=latest
+
+  echo '*******************************************************'
+  echo '***** Executing publishGhPages with PghPageType=version'
+  echo '*******************************************************'
   ./gradlew --no-daemon publishGhPages --rerun-tasks --info --stacktrace -PghPageType=version
 fi
