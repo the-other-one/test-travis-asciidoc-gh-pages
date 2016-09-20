f225ca770e29b8d162b5e0507f2c204275cdc5bb
Serban Iordache <iordache@hotmail.com>
2016-09-20 23:57:44 +0200

    remove debug messages

--- travis-build.sh
+++ travis-build.sh
@@ -3,20 +3,9 @@ set -ev
 ./gradlew --no-daemon build asciidoc
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" -a "${TRAVIS_BRANCH}" == "master" ]; then
-  echo "git ls-remote origin gh-pages = `git ls-remote origin gh-pages`"
   if [ "`git ls-remote origin gh-pages`" == "" ]; then
-    echo '****************************************************'
-    echo '***** Executing publishGhPages with PghPageType=init'
-    echo '****************************************************'
     ./gradlew --no-daemon publishGhPages --rerun-tasks --info --stacktrace -PghPageType=init
   fi
-  echo '******************************************************'
-  echo '***** Executing publishGhPages with PghPageType=latest'
-  echo '******************************************************'
   ./gradlew --no-daemon publishGhPages --rerun-tasks --info --stacktrace -PghPageType=latest
-
-  echo '*******************************************************'
-  echo '***** Executing publishGhPages with PghPageType=version'
-  echo '*******************************************************'
   ./gradlew --no-daemon publishGhPages --rerun-tasks --info --stacktrace -PghPageType=version
 fi
